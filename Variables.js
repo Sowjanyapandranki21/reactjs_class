f0552856e26cd5b5839eae9b83fa62c8f5e6418f
sowjanyapandranki21 <soujipandranki357@gmail.com>
2023-09-09 18:28:42 +0530

    firstcommit

---
+++ Variables.js
@@ -0,0 +1,11 @@
+//Declare,Assignment Operator,Assign Value
+//Assign Value later,Modify Existing
+
+let firstName="sowjanya";
+let address,state,zipcode;
+address="Hyderabad";
+state="TG";
+zipcode="532201";
+firstName="manju"
+console.log(address,state,zipcode);
+console.log(firstName)
